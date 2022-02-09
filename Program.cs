0ce41898877761110fce94cd95a0a9852b90407f
Doc <sidratgames@protonmail.com>
2022-02-09 14:55:37 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿bool isRaining = true;
+bool isRiding = true;
+
+if (isRaining)
+{Console.Writeline("I'm not going for a ride in the rain!");}
+
+else
+{Console.Writeline ("I can go for a ride");}
+
+Console.Writeline("How old are you?");
+string response = System.Console. Readline();
+int age = int.Parse(responce);
+
+string output = age >= 16 ? "You can ride!" : "You're too young to ride.";
+
+Console.Writeline (output);
